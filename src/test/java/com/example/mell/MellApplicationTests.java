fa32cb3f5fcbcb226123c49a5e93041205545de8
Frank <2691346668@qq.com>
2022-11-23 09:49:32 +0800

    测试

---
+++ src/test/java/com/example/mell/MellApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.mell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MellApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
